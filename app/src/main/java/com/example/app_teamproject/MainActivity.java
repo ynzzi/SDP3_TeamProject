65c9818895a403486e9218243268f5e14aee5e94
nowgnodeel123 <nowgnodeel123@gmail.com>
2023-06-01 17:28:46 +0900

    소개실3 상단 탭 작성.

---
+++ app/src/main/java/com/example/app_teamproject/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.app_teamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("정보"));
+        tabs.addTab(tabs.newTab().setText("지도"));
+        tabs.addTab(tabs.newTab().setText("만보기"));
+
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Log.d("MainActivity", "선택된 탭: " + position);
+
+                Fragment selected = null;
+                if(position == 0){
+                    selected = fragment1;
+                } else if(position == 1){
+                    selected = fragment2;
+                } else {
+                    selected = fragment3;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
